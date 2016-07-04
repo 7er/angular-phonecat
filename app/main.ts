1ff3f4c86bd27028a09fdd0505c2759a0b4f779e
Syver Enstad <syver.enstad@husbanken.no>
2016-07-04 18:48:04 +0200

    Ported phone-detail to angular 2 (animations don't work)

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,7 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import {Phone} from "./core/phone/phone.service";
 import {Type} from "@angular/core";
 import {PhoneListComponent} from "./phone-list/phone-list.component";
+import {PhoneDetailComponent} from "./phone-detail/phone-detail.component";
 
 
 let upgradeAdapter = new UpgradeAdapter();
@@ -10,7 +11,10 @@ upgradeAdapter.addProvider(HTTP_PROVIDERS);
 upgradeAdapter.addProvider(<Type>Phone);
 angular.module('core.phone').factory('phone', upgradeAdapter.downgradeNg2Provider(Phone));
 angular.module('phoneList')
-    .directive('phoneList',
+  .directive('phoneList',
     <angular.IDirectiveFactory>upgradeAdapter.downgradeNg2Component(<Type>PhoneListComponent));
-
+angular.module('phoneDetail')
+  .directive('phoneDetail',
+    <angular.IDirectiveFactory>upgradeAdapter.downgradeNg2Component(<Type>PhoneDetailComponent));
+upgradeAdapter.upgradeNg1Provider('$routeParams');
 upgradeAdapter.bootstrap(document.documentElement, ['phonecatApp']);
--- app/phone-detail/phone-detail.component.ts
+++ app/phone-detail/phone-detail.component.ts
@@ -1,10 +1,18 @@
 import {PhoneData, Phone} from "../core/phone/phone.service";
+import {Component, Inject, Type, Input, Output} from "@angular/core";
+import {CheckmarkPipe} from "../core/checkmark/checkmark.pipe";
 
-class PhoneDetailController {
+@Component({
+    selector: 'phone-detail',
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    pipes: [ <Type>CheckmarkPipe ]
+})
+export class PhoneDetailComponent {
     mainImageUrl: string;
     phone: PhoneData;
-    static $inject = ['$routeParams', 'phone'];
-    constructor($routeParams: angular.route.IRouteParamsService, phone: Phone) {
+    constructor(@Inject('$routeParams')
+                  $routeParams: angular.route.IRouteParamsService,
+                phone: Phone) {
         let phoneId = $routeParams['phoneId'];
         phone.get(phoneId).subscribe(data => {
             this.phone = data;
@@ -13,12 +21,7 @@ class PhoneDetailController {
     }
 
     setImage(imageUrl) {
+        console.log("setImage: ", imageUrl);
         this.mainImageUrl = imageUrl;
     }
 }
-
-// Register `phoneDetail` component, along with its associated controller and template
-angular.module('phoneDetail').component('phoneDetail', {
-    templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: PhoneDetailController
-});
