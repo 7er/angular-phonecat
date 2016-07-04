fcc359707f708d8a35e8fb738c388de5e421bc52
Syver Enstad <syver.enstad@husbanken.no>
2016-07-04 18:07:42 +0200

    Port angular 1 controller PhoneList to angular2 component

--- app/main.ts
+++ app/main.ts
@@ -2,11 +2,15 @@ import { UpgradeAdapter } from '@angular/upgrade';
 import { HTTP_PROVIDERS } from '@angular/http';
 import {Phone} from "./core/phone/phone.service";
 import {Type} from "@angular/core";
+import {PhoneListComponent} from "./phone-list/phone-list.component";
 
 
 let upgradeAdapter = new UpgradeAdapter();
 upgradeAdapter.addProvider(HTTP_PROVIDERS);
 upgradeAdapter.addProvider(<Type>Phone);
 angular.module('core.phone').factory('phone', upgradeAdapter.downgradeNg2Provider(Phone));
+angular.module('phoneList')
+    .directive('phoneList',
+    <angular.IDirectiveFactory>upgradeAdapter.downgradeNg2Component(<Type>PhoneListComponent));
 
 upgradeAdapter.bootstrap(document.documentElement, ['phonecatApp']);
